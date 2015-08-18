009f4df01cadc32fe7a6b3809174aa81735b7f05
Ian Henriksen <insertinterestingnamehere@gmail.com>
2015-08-18 11:25:25 -0600

    Combined value_count_in64 and value_count_float64 into a single routine using fused types.

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -244,10 +244,10 @@ def value_counts(values, sort=True, ascending=False, normalize=False,
 
         elif com.is_integer_dtype(dtype):
             values = com._ensure_int64(values)
-            keys, counts = htable.value_count_int64(values)
+            keys, counts = htable.value_count_scalar64(values, dropna)
         elif com.is_float_dtype(dtype):
             values = com._ensure_float64(values)
-            keys, counts = htable.value_count_float64(values, dropna)
+            keys, counts = htable.value_count_scalar64(values, dropna)
 
         else:
             values = com._ensure_object(values)
